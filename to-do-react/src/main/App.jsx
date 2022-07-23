47a5dc8c901e3dc3766583abfd7e615db9319384
LeonamOliveira <oliveira.leonam@hotmail.com>
2022-07-22 21:51:38 -0300

    Iniciando useState

--- to-do-react/src/main/App.jsx
+++ to-do-react/src/main/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import { Tarefa } from '../presentation/components/Tarefa'
 import Batata from '../presentation/components/Botao'
 import '../presentation/styles/App.css'
+
 export default function App() {
+  var lista = [];
+  const [valor, setValor] = useState("");
+  function HandleCreateTask(){
+    lista.append(valor)
+    console.log(lista)
+  }
   return (
     <>
     <div>
-      <input />
-      <Batata />
+      <input onChange={(e) => setValor(e.target.value)}/>
+      <button onClick={HandleCreateTask}/>
     </div>
     <div>
       <Tarefa />
